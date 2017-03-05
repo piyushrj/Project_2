b4f3655e12fd609ab76e6143eaf1dd392782495f
Piyush-sopho <raj.piyushn72@gmail.com>
2017-03-05 17:05:21 +0000

    Wrote the code for register form and its corresponding controller

--- views/register_form.php
+++ views/register_form.php
@@ -1,2 +1,35 @@
-register form like on the link http://project2.hol.es/register
-data from this form will go to register.php via post method
+<form action="register.php" method="post">
+    <fieldset>
+        
+        
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="email" placeholder="Email Address" type="text"/>
+        </div>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="name" placeholder="First Name" type="text"/>
+        </div>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="college" placeholder="College" type="text"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="password" placeholder="Password" type="password" />
+        </div>
+         <div class="form-group">
+            <input class="form-control" name="confirmation" placeholder="Password (again)" type="password" />
+        </div>
+        <div class="form-group">
+            <input type="radio" name="gender" value="male"> Male
+            <input type="radio" name="gender" value="female"> Female<br>
+
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+                Register
+            </button>
+        </div>
+    </fieldset>
+</form>
+<div>
+    or <a href="login.php">login</a>
+</div>
